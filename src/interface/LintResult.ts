acba5056fee00447ff0d61ea6b5cb6fb45d1da6e
chan haga <obutora@gmail.com>
2023-01-09 16:15:15 +0900

    add : lint card

---
+++ src/interface/LintResult.ts
@@ -0,0 +1,11 @@
+export default interface LintResult {
+  column: number; //当該行における何文字目[end]かを表す
+  index: number; //当該行における何文字目[start]かを表す
+  line: number;
+  loc: object;
+  message: string; //エラーメッセージ
+  range: number[];
+  ruleId: string; //ルールID
+  severaty: number; //エラーの重要度
+  type: string;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import Image from "next/image";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
@@ -11,10 +11,10 @@ import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 import { $getRoot, $getSelection } from "lexical";
 import ToolbarPlugin from "../editor/plugins/toolbar";
 import axios from "axios";
+import LintResult from "../interface/LintResult";
 
 function App() {
-  const [name, setName] = useState("");
-
+  const [lintResult, setLintResult] = useState<LintResult[]>([]);
   const [lineNum, setLineNum] = useState("1");
 
   const numRef = useRef(null);
@@ -59,18 +59,27 @@ function App() {
         </LexicalComposer> */}
       </div>
 
-      <div className="relative mx-auto h-96 w-9/12">
+      <div className="relative mx-auto h-96 w-full">
         <textarea
-          className="h-full w-full resize-none rounded-md p-4 pl-16 pb-4 outline-1 outline-indigo-500 focus:border-indigo-400"
+          className="col-span-9 h-full w-full resize-none rounded-md p-4 pl-16 pb-4 outline-1 outline-indigo-500 focus:border-indigo-400"
           wrap="off"
           ref={editorRef}
-          onChange={(e) => {
+          onChange={async (e) => {
             const lines = e.currentTarget.value.split(/\n/).length;
             const lineNumArray = Array.from(
               Array(lines),
               (_, index) => index + 1 + `\n`
             );
             setLineNum(lineNumArray.join(""));
+
+            const res = await axios.post("http://localhost:1420/api/lint", {
+              inputText: e.currentTarget.value,
+            });
+
+            const result = res.data.text as LintResult[];
+            setLintResult(result);
+
+            console.log(result);
           }}
         ></textarea>
         <textarea
@@ -79,15 +88,28 @@ function App() {
           ref={numRef}
           value={lineNum}
         ></textarea>
-      </div>
 
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
+        <div className="lint-card grid w-full grid-cols-3 gap-4">
+          {lintResult.map((lint, index) => {
+            return (
+              <div
+                key={
+                  lint.column.toString() +
+                  lint.index.toString() +
+                  index.toString()
+                }
+                className={
+                  "flex items-center rounded-lg border border-indigo-200 bg-white shadow-md shadow-indigo-50"
+                }
+              >
+                <div className="mb-2 p-2">
+                  <p className="mb-1 rounded-sm bg-indigo-50 p-1 text-sm text-indigo-700">{`${lint.line}行目`}</p>
+                  <p className="rounded-sm bg-indigo-50 p-1 text-sm text-indigo-700">{`${lint.index}-${lint.column}文字目`}</p>
+                </div>
+                <p className="ml-2 text-sm">{lint.message}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
