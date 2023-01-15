13ed9f19fa0161babe07d59c4bcc0fb174a1933c
chan haga <obutora@gmail.com>
2023-01-15 14:49:34 +0900

    fix : font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/component/**/*.{js,ts,jsx,tsx}",
     // "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+
+        sans: ["Inter", "Noto Sans JP", "sans-serif"],
+        mono: ["Inter", "monospace"],
+      }
+    },
   },
   plugins: [],
 }
